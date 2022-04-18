3da96fef84ee74b2e5f387a835d842099adc8769
Saffet Gokcen Sen <saffetgokcensen@hotmail.com>
2022-04-18 23:53:49 +0300

    defined the objects for all of the buttons of the calculator

---
+++ javascript/calculator.js
@@ -0,0 +1,20 @@
+const division = document.getElementById("division");
+const multiplication = document.getElementById("multiplication");
+const subtraction = document.getElementById("subtraction");
+const addition = document.getElementById("addition");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const zero = document.getElementById("zero");
+const one = document.getElementById("one");
+const dot = document.getElementById("dot");
+const equals = document.getElementById("equals");
+const clear = document.getElementById("clear");
+const backspace = document.getElementById("backspace");
+
+
